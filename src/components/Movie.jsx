16f8acafc5b32334b68f5f784ea7c5355a3bc525
fnicom <fnicomedes@gmail.com>
2022-04-21 20:32:16 -0300

    estrutura inicial

---
+++ src/components/Movie.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Movie = () => {
+  return (
+    <div className='movie'>
+        
+        This is a componente of movies
+        
+    </div>
+  )
+}
+
+export default Movie
