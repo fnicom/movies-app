eb2108ce0875ed95b523a54553b84b8af9390868
fnicom <fnicomedes@gmail.com>
2022-04-23 09:30:29 -0300

    ajustes cards movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Movie from './components/Movie.jsx'
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
+
+  const [ movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+    .then((res) => res.json()).then((data) => {
+      console.log(data, 'teste')
+      setMovies(data.results)
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Olá Mundo</h1>
+    <div className="movie-container">
+      {movies.length > 0 && movies.map((movie) => 
+        <Movie key={movie.id} {...movie} /> 
+      )}
     </div>
   );
 }
